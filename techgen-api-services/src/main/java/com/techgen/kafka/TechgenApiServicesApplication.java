942ffce35b814e0c63a82aa4aa74a86ec5d7d793
Taher <Dheeraj117@github.com>
2023-08-14 07:39:39 +0530

    Add spring boot kafka multi module project for library event

---
+++ techgen-api-services/src/main/java/com/techgen/kafka/TechgenApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.techgen.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechgenApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechgenApiServicesApplication.class, args);
+	}
+
+}
